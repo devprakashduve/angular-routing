b92a92dc0f0657a5885457c3689413c789ed6b64
devprakashduve <dev786.1@gmail.com>
2019-12-12 01:25:09 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 
 import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
@@ -11,7 +11,7 @@ import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,appRoutingModule ],
+  imports:      [ BrowserModule, FormsModule,ReactiveFormsModule ,appRoutingModule ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, LoginComponent, RegisterComponent ],
   bootstrap:    [ AppComponent ]
 })
